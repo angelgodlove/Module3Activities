d3919f29b85933546a82c3d9eb07ef7c97885da6
Angel Godlove <angelgodlove@my.whccd.edu>
2025-01-29 19:17:02 -0800

    Add project files.

---
+++ LectureActivity1-29-2025/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Design;
+
+namespace LectureActivity1_29_2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter any day: ");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+
+            {
+                Console.WriteLine("The number you entered is a zero.");
+                Console.WriteLine("The number you entered is Even.");
+            }
+            else if (num == 0)
+            
+            {
+                Console.WriteLine("The number you entered is Odd.");
+                else if (num > 0) ;
+                Console.WriteLine("I don't like evaluating negative numbers.");
+
+
+            }
+
+
+            
+            
+
+         }   
+    }
+}
+    
